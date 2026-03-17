aa10d7ead7a9f7318f7039ec823cf8902a51b257
ahanaf607307 <ahanaf607307@gmail.com>
2026-03-17 15:39:20 +0600

    added public experience api

--- src/app/modules/experience/experience.route.js
+++ src/app/modules/experience/experience.route.js
@@ -10,6 +10,7 @@ const router = express.Router();
 router.post("/", checkAuthMiddleware(...Object.values(Role)), ExperienceController.createExperience);
 router.get("/", checkAuthMiddleware(...Object.values(Role)), ExperienceController.getAllExperiences);
 router.get("/user/:userId", checkAuthMiddleware(...Object.values(Role)), ExperienceController.getExperiencesByUserId);
+router.get("/user/for-ai/:userId",  ExperienceController.getExperiencesByUserId);
 router.get("/:id", checkAuthMiddleware(...Object.values(Role)), ExperienceController.getExperienceById);
 router.patch("/:id", checkAuthMiddleware(...Object.values(Role)), ExperienceController.updateExperience);
 router.delete("/:id", checkAuthMiddleware(...Object.values(Role)), ExperienceController.deleteExperience);
